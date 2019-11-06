73b9487c4d349801f79fe39bff1472c6d7e67ef1
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-11-06 13:27:40 -0500

    add combinationOfsum exercice

--- combinationOfSums.js
+++ combinationOfSums.js
@@ -26,6 +26,19 @@
 // What if there are negative numbers?
 
 function findPairsInArrayWithSum(array, sum) {
-    // Add code
+    let answerArray = []
+    for(let i = 0; i <= array.length-2; i++) {
+      for (let j = i+1; j <= array.length-1; j++) {
+        if (array[i] + array[j] === sum) {
+          answerArray.push([array[i], array[j]])
+        }
+      }
+    }
+    return answerArray
 }
+console.log (findPairsInArrayWithSum([3, 3, 3, 5, 1, 6, 0], 6))
+console.log (findPairsInArrayWithSum([3, 3, 3, 5, 1, 6, 0, 4, 4, 3, 7, 1, 5, 1], 8))
 
+// create an empty array that will be pairs
+// loop through the array twice, to test 1 number with all the numbers remaining
+// if the pairs = 0, push them into the array
